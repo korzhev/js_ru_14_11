21ccb83e29ed01563ef24c6779d516bc7c9b4d88
Vladimir <amv12dbr9@gmail.com>
2016-11-20 16:48:15 +0300

    fix css

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { articles } from './fixtures';
 import ArticleList from './components/ArticleList';
-// import '../node_modules/semantic-ui-css/semantic.min.css';
+import '../node_modules/semantic-ui-css/semantic.min.css';
 
 render(<ArticleList articles={articles} />, document.getElementById('container'));
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,17 @@ module.exports = {
         loaders: ['babel'],
         include: path.join(__dirname, 'src'),
       },
-      // { test: /\.css/, loader: "style-loader!css-loader" },
+      // { test: /\.css/, loader: ExtractTextPlugin.extract("style-loader!css-loader") },
+      { test: /\.css/, loader: 'style-loader!css-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      {
+        test: /\.png$/,
+        loader: 'file?name=[name].[ext]',
+      }, {
+        test: /\.jpg$/,
+        loader: 'file?name=[name].[ext]',
+      }
     ],
   },
   // plugins: [
