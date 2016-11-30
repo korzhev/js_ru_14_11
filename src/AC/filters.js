28ea9de2b709b093e5295448bfc6f25e6419eb3b
Roman <kaboom.ua@gmail.com>
2016-11-30 19:18:28 +0200

    Update filters.js

--- src/AC/filters.js
+++ src/AC/filters.js
@@ -1,5 +1,6 @@
 import { FILTER_ARTICLE } from '../constants'
 
+//лучше разбей на 2 AC, ведь это 2 дазных типа фильтров
 export function filterArticle(id, from, to) {
     let payload ={}
     if (id !== undefined) payload['id']=id
