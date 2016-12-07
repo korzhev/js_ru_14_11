2094b032d7db2b77bb6ab442f8df805b044e9ed6
Roman <kaboom.ua@gmail.com>
2016-12-07 22:44:45 +0200

    Update idGeneration.js

--- src/middlewares/idGeneration.js
+++ src/middlewares/idGeneration.js
@@ -3,8 +3,11 @@ import { ADD_COMMENT } from '../constants'
 let globalCommentId = 0
 
 export default store => next => action => {
+    //лучше придумать более общий способ; через мидлвару будут проходить все экшины, их стоит делать максимально реюзабельными
   switch (action.type) {
     case ADD_COMMENT:
+      //Плохая логика для генерации id
+      //лучше не мутировать объект
       action.payload['id'] = globalCommentId ? globalCommentId++ : store.getState().comments.maxBy(c => c.id).id + 1
   }
   next(action)
